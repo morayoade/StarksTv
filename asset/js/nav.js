fcfd4dcca2bc184daf4aab68a5569351535d6d8a
morayoade <morayoadelekan@starksltd.com>
2021-07-15 09:57:01 +0000

    starksTv updated

---
+++ asset/js/nav.js
@@ -0,0 +1,27 @@
+const navToggle=document.querySelector(".nav-togglericon");
+const menuBars=document.querySelector(".nav-bars");
+const menuTimes=document.querySelector(".nav-times");
+const navDropdrown=document.querySelector(".navbarDropdown");
+let showMenu=false;
+
+navToggle.addEventListener('click', toggleNavbar);
+function toggleNavbar(){
+    if(!showMenu){
+        // menuBars.classList.add("menuTimes")
+        menuBars.style.display="none";
+        menuTimes.style.display="block";
+        navDropdrown.style.display="block";
+
+    //    reset showMenu
+    showMenu=true;
+     }
+      else {
+        menuBars.style.display="block";
+        menuTimes.style.display="none";
+        navDropdrown.style.display="none";
+
+        showMenu=false;
+
+    }
+    
+}
