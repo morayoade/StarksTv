35540b51aad5c03476bd2c1df286e0bf4190584d
morayoade <morayoadelekan@starkltd.com>
2021-11-25 13:36:45 +0100

    updated

--- asset/js/nav.js
+++ asset/js/nav.js
@@ -4,6 +4,18 @@ const menuTimes=document.querySelector(".nav-times");
 const navDropdrown=document.querySelector(".navbarDropdown");
 const navHeading=document.querySelector(".navbar-heading");
 
+// var btns = navHeading.getElementsByClassName("navbar-active");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   var current =document.querySelector(".activeclass");
+// //   current[0].className = current[0].className.replace(" activeclass", "");
+// //   this.className += "activeclass";
+//   });
+// }
+
+
+// const menuItem=document.querySelectorAll("navbar-active")
+// const m 
 let showMenu=false;
 
 navToggle.addEventListener('click', toggleNavbar);
@@ -35,3 +47,9 @@ window.onscroll=()=>{
         navHeading.classList.remove("nav-active")
     }
 }
+const activePage=window.location.pathname;
+const navLinks=document.querySelectorAll(".navbar-active ").forEach(link=>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add("activeclass")
+    }
+})
